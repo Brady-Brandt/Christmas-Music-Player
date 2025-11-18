5aaadeacff89d2a73e896f5d6492840778bcdc5d
Brady-Brandt <bradybrandt3@gmail.com>
2025-11-18 12:48:35 -0600

    Found song list

--- audio.py
+++ audio.py
@@ -1,115 +1,71 @@
+from collections import Counter
+
 import mido
+import os
 import math
 
 
+class Song:
+    def __init__(self, file_name: str, name: str, channel: int):
+        self.file_path = "songs" + os.path.sep + file_name
+        self.name = name 
+        self.channel = channel
+
+songs = []
+songs.append(Song("twelvedays.mid", "12 Days of Xmas", 6))
+songs.append(Song("carolofbells.mid", "Carol of the Bells", 0))
+songs.append(Song("jinglebells.mid", "Jingle Bells", 0))
+songs.append(Song("Allwantisyou.mid", "All I want for Xmas is you", 3))
+songs.append(Song("rockingtree.mid", "Rocking Around Xmas Tree", 0)) # doesn't sound bad if we filter freq < 250 and don't allow rests
+songs.append(Song("frosty.mid", "Frosty the Snowman", 1))  #allow rests
+songs.append(Song("rudolph.mid", "Rudolph", 0))  #needs work sounds okay without rests
+songs.append(Song("letitsnow.mid", "Let it snow", 6))  
+songs.append(Song("bellrock.mid", "Jingle Bell Rock", 3))  #needs work sounds okay with rests
+songs.append(Song("herecomes.mid", "Here Comes Santa Claus", 0)) #rests
+songs.append(Song("hollyjolly.mid", "Holly Jolly Xmas", 6)) #sounds okay with rests
+songs.append(Song("wishyou.mid", "We Wish you a merry Xmas", 0))
+songs.append(Song("sleighride.mid", "Sleigh Ride", 2))
+
+
+
 notes = [
-8.18, 8.66, 9.18, 9.72, 10.30, 10.91, 11.56, 12.25,
-12.98, 13.75, 14.57, 15.43, 16.35, 17.32, 18.35,
-19.45, 20.60, 21.83, 23.12, 24.50, 25.96, 27.50,
-29.14, 30.87, 32.70, 34.65, 36.71, 38.89, 41.20,
-43.65, 46.25, 49.00, 51.91, 55.00, 58.27, 61.74, 
-65.41, 69.30, 73.42, 77.78, 82.41, 87.31, 92.50, 
-98.00, 103.83, 110.00, 116.54, 123.47, 130.81, 
-138.59, 146.83, 155.56, 164.81,
-174.61,
-185.00,
-196.00,
-207.65,
-220.00,
-233.08,
-246.94,
-261.63,
-277.18,
-293.66,
-311.13,
-329.63,
-349.23,
-369.99,
-392.00,
-415.30,
-440.00,
-466.16,
-493.88,
-523.25,
-554.37,
-587.33,
-622.25,
-659.26,
-698.46,
-739.99,
-783.99,
-830.61,
-880.00,
-932.33,
-987.77,
-1046.50,
-1108.73,
-1174.66,
-1244.51,
-1318.51,
-1396.91,
-1479.98,
-1567.98,
-1661.22,
-1760.00,
-1864.66,
-1975.53,
-2093.00,
-2217.46,
-2349.32,
-2489.02,
-2637.02,
-2793.83,
-2959.96,
-3135.96,
-3322.44,
-3520.00,
-3729.31,
-3951.07,
-4186.01,
-4434.92,
-4698.64,
-4978.03,
-5274.04,
-5587.65,
-5919.91,
-6271.93,
-6644.88,
-7040.00,
-7458.62,
-7902.13,
-8372.02,
-8869.84,
-9397.27,
-9956.06,
-10548.08,
-11175.30,
-11839.82,
-12543.85
+8.18, 8.66, 9.18, 9.72, 10.30, 10.91, 11.56, 12.25, 12.98, 13.75,
+14.57, 15.43, 16.35, 17.32, 18.35, 19.45, 20.60, 21.83, 23.12, 24.50,
+25.96, 27.50, 29.14, 30.87, 32.70, 34.65, 36.71, 38.89, 41.20, 43.65, 
+46.25, 49.00, 51.91, 55.00, 58.27, 61.74, 65.41, 69.30, 73.42, 77.78, 
+82.41, 87.31, 92.50, 98.00, 103.83, 110.00, 116.54, 123.47, 130.81, 138.59,
+146.83, 155.56, 164.81, 174.61, 185.00, 196.00, 207.65, 220.00, 233.08, 246.94,
+261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392.00, 415.30, 440.00,
+466.16, 493.88, 523.25, 554.37, 587.33, 622.25, 659.26, 698.46, 739.99, 783.99,
+830.61, 880.00, 932.33, 987.77, 1046.50, 1108.73, 1174.66, 1244.51, 1318.51, 1396.91, 
+1479.98, 1567.98, 1661.22, 1760.00, 1864.66, 1975.53, 2093.00, 2217.46, 2349.32, 2489.02, 
+2637.02, 2793.83, 2959.96, 3135.96, 3322.44, 3520.00, 3729.31, 3951.07, 4186.01, 4434.92,
+4698.64, 4978.03, 5274.04, 5587.65, 5919.91, 6271.93, 6644.88, 7040.00, 7458.62, 7902.13,
+8372.02, 8869.84, 9397.27, 9956.06, 10548.08, 11175.30, 11839.82, 12543.85 
 ]
 
 
-
-mid = mido.MidiFile('alla-turca.mid')
+current_song = songs[12]
+mid = mido.MidiFile(current_song.file_path)
 
 
 ticks_per_beat = mid.ticks_per_beat
 tempo = 0
 
 channel1 = 0
-channels = [0 for i in range(16)]
 
 channel1_data = []
 
 class AudioData:
-    def __init__(self, freq: float, start: int, duration: int, velocity: int, time_duration: float):
+    def __init__(self, freq: float, start: int, duration: int, velocity: int, tempo: int, time_duration: float):
         self.freq = freq
-        self.start = start 
-        self.duration = duration 
+        self.start = start # start time in ticks
+        self.duration = duration  # duration in ticks
         self.velocity = velocity 
-        self.time_duration = time_duration
+        self.time_duration = time_duration #number of pic clocks
+        self.tempo = tempo
     def __str__(self):
-       return f"freq: {self.freq}, start: {self.start}, duration: {self.duration}, vel: {self.velocity}" 
+       return f"freq: {self.freq}, start: {self.start}, duration: {self.duration}, time: {self.time_duration}" 
         
 
 c1_time = 0
@@ -119,19 +75,24 @@ active_c1_notes = {}
 
 merged = mido.merge_tracks(mid.tracks)
 
+channels = [0 for i in range(16)]
+
 current_ticks = 0
 
 for msg in merged:
     current_time += mido.tick2second(msg.time, ticks_per_beat, tempo)
     current_ticks += msg.time
 
+
     if msg.type == 'set_tempo':
         tempo = msg.tempo
 
+    if msg.type == "note_on":
+        channels[msg.channel] += 1
 
     if msg.type == 'note_on' and msg.velocity > 0:
         # Start of a note
-        if msg.channel == 0 or msg.channel == 12:
+        if msg.channel == current_song.channel:
             active_c1_notes[msg.note] = (current_ticks, msg.velocity)
 
     elif (msg.type == 'note_off') or (msg.type == 'note_on' and msg.velocity == 0):
@@ -139,7 +100,7 @@ for msg in merged:
             (start, velocity) = active_c1_notes.pop(msg.note)
             duration = current_ticks - start
             freq = notes[msg.note]
-            channel1_data.append(AudioData(freq, start, duration, velocity, (tempo * duration * 10) / (ticks_per_beat))) 
+            channel1_data.append(AudioData(freq, start, duration, velocity, tempo, (tempo * duration * 10) / (ticks_per_beat))) 
 
 for i, track in enumerate(mid.tracks):
     print(f"Track {i}: {track.name}")
@@ -147,8 +108,10 @@ for i, track in enumerate(mid.tracks):
         if msg.type == "program_change":
             print(f"  Channel {msg.channel}, Program {msg.program}")
 
+channel1_data.sort(key= lambda d: d.start)
+print(channels)
 
-def remove_chords(channel_data, time_tolerance=0.05):
+def remove_chords(channel_data):
     melody = []
 
     for note in channel_data:
@@ -173,6 +136,10 @@ def remove_chords(channel_data, time_tolerance=0.05):
 
         # notes don't overlap
         if start > prev_end:
+            # handle rests
+            if start - prev_end > tick_tolerance and True:
+                tempo = melody[-1].tempo
+                melody.append(AudioData(40000, 0,0,0, tempo, (tempo * (start - prev_end) * 10) / (ticks_per_beat)))
             melody.append(note)
             continue
 
@@ -185,7 +152,7 @@ def remove_chords(channel_data, time_tolerance=0.05):
                 melody[-1] = note
             continue
 
-
+        # some notes appear to overlap even though they don't
         if start - tick_tolerance <= prev_end:
             melody.append(note)
             continue
@@ -194,18 +161,17 @@ def remove_chords(channel_data, time_tolerance=0.05):
 
 
 print(f"Song Length: {int(current_time)} seconds")
-new_channel1 = remove_chords(channel1_data, 1e-6)
+new_channel1 = remove_chords(channel1_data)
+
+
+print(len(channel1_data), len(new_channel1))
 
 for i in range(50):
     print(channel1_data[i], new_channel1[i])
 
-print(len(channel1_data), len(new_channel1))
-
 
 def get_timer0_prescalar(data):
-    CLOCK_SPEED = 10_000_000
     UINT16_MAX = 65535
-    clock = lambda data, ps: int(data.time_duration * CLOCK_SPEED / ps)
     if data.time_duration < UINT16_MAX:
         return (data.time_duration, 0x88)
     elif data.time_duration / 2 < UINT16_MAX:
@@ -250,6 +216,8 @@ with open("audio.h", "w") as f:
         f.write(f"{int(clocks)},\n") 
     f.write('};\n')
 
+    print(f"Duration Prescalars: {Counter(prescalars)}")
+
     f.write(f"const unsigned char c1_prescalars[{len(new_channel1)}] = {{")
     for scalar in prescalars:
         f.write(f"{scalar},\n") 
@@ -264,6 +232,9 @@ with open("audio.h", "w") as f:
         prescalars.append(prescalar)
         f.write(f"{clocks},\n") 
     f.write('};\n')
+
+
+    print(f"Duration Prescalars: {Counter(prescalars)}")
 
 
     f.write(f"const unsigned char c1_notes_scalars[{len(new_channel1)}] = {{")
